6c1bb539f656c5f0d084a28f84d9390fd7fd77b1
Jorge <122285477+salasPINE@users.noreply.github.com>
2023-02-13 15:10:05 +0000

    Added up to ex12 and the input proficiency check. Added ex26.

---
+++ pcinput_salas.py
@@ -0,0 +1,15 @@
+# Jorge Salas
+# Proficency Check Input
+# pcinput_salas.py
+
+print("You will be asked five questions by the computer. Type in your answer and press the return key after each answer")
+print()
+hair_color = input("What is your hair color? ")
+color = input("What is your favorite color? ")
+city = input("What city were you born in? ")
+live = input("What city do you live in now? ")
+song = input("What is your favorite song of all time? ")
+print()
+print(f"Your hair color is {hair_color}. Your favorite color, however, is {color}.")
+print(f"You were born in {city}, and you now live in {live}.")
+print(f"Your favorite song is {song}.")
