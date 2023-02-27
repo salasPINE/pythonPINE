70a4208ffc0242e0825a5070625a33d28e07c462
Jorge <122285477+salasPINE@users.noreply.github.com>
2023-02-27 18:54:13 +0000

    organized into folders

--- functions_salas.py
+++ functions_salas.py
@@ -8,11 +8,13 @@ def salas_function():
 salas_function()
 
 def salas_function_arg(arg1, arg2, arg3, arg4):
-  print(f"arg1: {arg1}, arg2: {arg2}, arg3: {arg3}, arg4: {arg4}")
-
-salas_function_arg("One","Two","Three","Four")
-
-def salas_function_arg_clean(arg1, arg2, arg3, arg4):
   print(f"{arg1}, {arg2}, {arg3}, {arg4}")
 
-salas_function_arg_clean("One","Two","Three","Four")
+salas_function_arg("One","Two","Three","Four")
+salas_function_arg("1","2","3","4")
+salas_function_arg("Uno","Dos","Tres","Cuatro")
+
+def salas_function_arg_desc(arg1, arg2, arg3, arg4):
+  print(f"arg1: {arg1}, arg2: {arg2}, arg3: {arg3}, arg4: {arg4}")
+
+salas_function_arg_desc("One","Two","Three","Four")
--- main_import.py
+++ main_import.py
@@ -1,9 +1,10 @@
 # Use the import function to run a file that is not in main.py
 # You can import multiple files
+# If the file is in a folder. Add that before a "period"
 # Do not include the .py extension
 
-# import MSCode.ex1_salas
-# import Turtle.tDefault
-# import ex0a_salas
+# import turtleCode.tDefault
+# import pythonExercises.ex0_salas
+# import ex0_salas
 
-import ex1_salas
+import pythonExercises.ex1_salas
